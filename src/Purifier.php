1f230cabfbee7680d07eed2ee585298094f87dce
Luke Towers <luke.towers@lookagency.com>
2017-01-11 17:20:27 -0600

    Adds support for HTML5 attributes and elements extension of Purifier (Based off of https://github.com/kennberg/php-htmlpurfier-html5) and support for adding in custom attributes and elements to support via the configuration. Fully backwards compatible.

--- src/Purifier.php
+++ src/Purifier.php
@@ -75,10 +75,119 @@ class Purifier
         }
 
         $config->loadArray($this->getConfig());
+        
+        // Load custom definition if set
+        if ($definitionConfig = $this->config->get('purifier.settings.custom_definition')) {
+	        $this->addCustomDefinition($definitionConfig, $config);
+        }
+        
+        // Load custom elements if set
+        if ($elements = $this->config->get('purifier.settings.custom_elements')) {
+	        if ($def = $config->maybeGetRawHTMLDefinition()) {
+		        $this->addCustomElements($elements, $def);
+	        }
+        }
+        
+        // Load custom attributes if set
+        if ($attributes = $this->config->get('purifier.settings.custom_attributes')) {
+	        if ($def = $config->maybeGetRawHTMLDefinition()) {
+		        $this->addCustomAttributes($attributes, $def);
+	        }
+        }
 
         // Create HTMLPurifier object
         $this->purifier = new HTMLPurifier($this->configure($config));
     }
+    
+    /**
+	 * Add a custom definition
+	 *
+	 * @see http://htmlpurifier.org/docs/enduser-customize.html
+	 * @param array $definitionConfig
+	 * @param HTML_Purifier_Config $configObject Defaults to using default config
+	 * 
+	 * @return HTML_Purifier_Config $configObject
+	 */
+	private function addCustomDefinition(array $definitionConfig, $configObject = null)
+	{
+		if (!$configObject) {
+			$configObject = HTMLPurifier_Config::createDefault();
+			$configObject->loadArray($this->getConfig());
+		}
+		
+		// Setup the custom definition
+		$configObject->set('HTML.DefinitionID', $definitionConfig['id']);
+		$configObject->set('HTML.DefinitionRev', $definitionConfig['rev']);
+		
+		// Enable debug mode
+		if (!isset($definitionConfig['debug']) || $definitionConfig['debug']) {
+			$configObject->set('Cache.DefinitionImpl', null);
+		}
+		
+		// Start configuring the definition
+		if ($def = $configObject->maybeGetRawHTMLDefinition()) {
+			// Create the definition attributes
+			if (!empty($definitionConfig['attributes'])) {
+				$this->addCustomAttributes($definitionConfig['attributes'], $def);
+			}
+			
+			// Create the definition elements
+			if (!empty($definitionConfig['elements'])) {
+				$this->addCustomElements($definitionConfig['elements'], $def);
+			}
+		}
+		
+		return $configObject;
+	}
+	
+	/**
+	 * Add provided attributes to the provided definition
+	 *
+	 * @param array $attributes
+	 * @param HTMLPurifier_HTMLDefinition $definition
+	 * 
+	 * @return HTMLPurifier_HTMLDefinition $definition
+	 */
+	private function addCustomAttributes(array $attributes, $definition)
+	{
+		foreach ($attributes as $attribute) {
+			// Get configuration of attribute
+			$required = !empty($attribute[3]) ? true : false;
+			$onElement = $attribute[0];
+			$attrName = $required ? $attribute[1] . '*' : $attribute[1];
+			$validValues = $attribute[2];
+			
+			$definition->addAttribute($onElement, $attrName, $validValues);
+		}
+		
+		return $definition;
+	}
+	
+	/**
+	 * Add provided elements to the provided definition
+	 *
+	 * @param array $elements
+	 * @param HTMLPurifier_HTMLDefinition $definition
+	 * 
+	 * @return HTMLPurifier_HTMLDefinition $definition
+	 */
+	private function addCustomElements(array $elements, $definition)
+	{
+		foreach ($elements as $element) {
+			// Get configuration of element
+			$name = $element[0];
+			$contentSet = $element[1];
+			$allowedChildren = $element[2];
+			$attributeCollection = $element[3];
+			$attributes = isset($element[4]) ? $element[4] : null;
+			
+			if (!empty($attributes)) {
+				$definition->addElement($name, $contentSet, $allowedChildren, $attributeCollection, $attributes);
+			} else {
+				$definition->addElement($name, $contentSet, $allowedChildren, $attributeCollection);
+			}
+		}
+	}
 
     /**
      * Check/Create cache directory
